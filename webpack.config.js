ff87e7ac1b5c4f94540dc81c93106c6eb9c80c2c
astrobass <mygithub@wtree.com>
2019-08-11 11:22:31 -0400

    Testing pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: ['./src/svgdash.js',
-  	'./src/DialHalfBasic.js',
-  	'./src/DialFullBasic.js',
-  	'./src/DialFullBeamer.js'],
+  entry: ['./src/svgdash.js'],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'svgdash.js',
